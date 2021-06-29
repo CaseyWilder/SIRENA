0ab0e453b102af4df188e2f4c5b20c8a459f8da0
thangnguyen-novobi <84591142+thangnguyen-novobi@users.noreply.github.com>
2021-06-28 22:44:05 -0600

    [SRN-30] Create default data for 3 companies

---
+++ novobi_addons/sirena_base/__manifest__.py
@@ -0,0 +1,11 @@
+{
+ 'name': "Sirena Base Module",
+ 'summary': "Contains customized code for Sirena",
+ 'author': "Novobi LLC",
+ 'category': 'Others',
+ 'version': '14.1.0.0',
+ 'depends': ['base'],
+ 'data': [
+  'data/res_company_override.xml',
+ ],
+}
---
+++ novobi_addons/sirena_product/__manifest__.py
@@ -0,0 +1,12 @@
+{
+ 'name': "Sirena Product Template",
+ 'summary': "Modifies/Adds new fields to the Product Template model",
+ 'description': "Adds some customized fields for the product.template model",
+ 'author': "Novobi LLC",
+ 'category': 'Product',
+ 'version': '14.1.0.0',
+ 'depends': ['base','product'],
+ 'data': [
+  'views/product_template_form_inherited.xml',
+ ],
+}
