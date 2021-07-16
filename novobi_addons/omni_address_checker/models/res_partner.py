25adc58a24b5587889e48f256d6cb9cd7348836b
Thang Nguyen <thang.nguyen@novobi.com>
2021-07-16 10:43:09 -0600

    Correct fields in res_partner

--- novobi_addons/omni_address_checker/models/res_partner.py
+++ novobi_addons/omni_address_checker/models/res_partner.py
@@ -19,10 +19,10 @@ class ResPartner(models.Model):
             'street2': validated_address.StreetLines[1] or '',
             'city': validated_address.City,
             'zip': validated_address.PostalCode,
-            'state': next((state for state in self.env['res.country.state'].search([])
-                           if state.code == validated_address.StateOrProvinceCode), None),
-            'country': next((country for country in self.env['res.country'].search([])
-                             if country.code == validated_address.CountryCode), None)
+            'state_id': next((state for state in self.env['res.country.state'].search([])
+                              if state.code == validated_address.StateOrProvinceCode), None),
+            'country_id': next((country for country in self.env['res.country'].search([])
+                                if country.code == validated_address.CountryCode), None)
         })
 
     @api.depends('street', 'street2', 'city', 'state_id', 'country_id', 'zip')
