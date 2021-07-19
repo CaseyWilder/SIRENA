51e38f34041c58604bcc08da49e976bcee3e8c8f
Thang Nguyen <thang.nguyen@novobi.com>
2021-07-19 17:43:30 -0600

    [SRN-73] Purchase Order report customization

---
+++ novobi_addons/sirena_purchase/models/purchase_order_line.py
@@ -0,0 +1,28 @@
+import os
+from odoo import models, fields, api
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    # Expose these product.template fields so that they could be displayed in report.
+    part_name_in_chinese = fields.Char(string="Part Name in Chinese",
+                                       related='product_id.product_tmpl_id.part_name_in_chinese')
+    drawing_number = fields.Char(string="Drawing Number",
+                                 related='product_id.product_tmpl_id.drawing_number')
+    substance = fields.Char(string="Substance",
+                            related='product_id.product_tmpl_id.substance')
+    parts_spec_color = fields.Char(string="Parts Spec / Color",
+                                   related='product_id.product_tmpl_id.parts_spec_color')
+    part_code = fields.Char(string="Part Code",
+                            related='product_id.product_tmpl_id.part_code')
+    product_image_1920 = fields.Image(max_width=80, max_height=50, related='product_id.image_1920')
+
+    @api.model
+    def install_cn_font(self):
+        print('\n\n\n' + 'Inside auto executed function!' + '\n\n\n')
+        # Potential risks:
+        # - Packages could be unregistered and replaced with harmful packages.
+        # - Roughly 25 MB of storage will be consumed.
+        result = os.system("apt-get install -y ttf-wqy-microhei ttf-wqy-zenhei")
+        print('\n\n\n' + str(result) + '\n\n\n')
