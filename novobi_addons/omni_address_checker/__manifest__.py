bd241bb76286e3f2fec1758c3d9e4abd2407ab5b
Tien Nguyen <tien.nguyen@novobi.com>
2021-07-22 22:58:51 +0700

    [SRN-59] Validate shipping address (Model + View) (#16)
    * [SRN-50 & SRN-51] Added support for Home Delivery and SmartPost FedEx shipping services.
    
    * Reformed
    
    * Minor fixes
    
    * Minor fix: include is_residential_address into update()
    
    * [SRN-59] Added the omni_address_checker module's backend part
    
    * [SRN-59] Added backend models for FedEx Address Validation.
    
    * Minor fix
    
    * Reformed and refactored code structure
    
    * Correct fields in res_partner
    
    * Fixed bug where there could be multiple states that have the same 2-letter code.
    
    * [SRN-73] Purchase Order report customization
    
    * [SRN-59] Validate shipping address (Model + View)
    
    * Finished omni_address_checker.
    Updated Shipping Information tab.
    
    * Refactored code and adjusted icons' views.
    
    Co-authored-by: Thang Nguyen <thang.nguyen@novobi.com>

--- novobi_addons/omni_address_checker/__manifest__.py
+++ novobi_addons/omni_address_checker/__manifest__.py
@@ -1,19 +1,36 @@
-# Copyright © 2020 Novobi, LLC
+# Copyright © 2021 Novobi, LLC
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Address Checker',
-    'version': '13.1.0',
+    'name': 'Shipping Integration: Address Checker',
+    'summary': 'Validate and auto correct delivery address using FedEx',
+    'description': 'Validate and auto correct delivery address using FedEx',
+    'version': '14.1.0.0',
     'category': 'Other',
-    'author': 'Novobi',
-    "license": "OPL-1",
+    'author': 'Novobi LLC',
+    'license': 'OPL-1',
     'website': 'https://www.novobi.com',
-    'depends': [
-        'omni_fedex'
-    ],
+    'depends': ['omni_fedex'],
     'data': [
+        # ============================== DATA =================================
+
+        # ============================== SECURITY =============================
+        'security/ir.model.access.csv',
+
+        # ============================== VIEWS ================================
+        'views/stock_picking_views.xml',
+
+        # ============================== REPORT ===============================
+
+        # ============================== WIZARD ===============================
+        'wizard/address_validation_wizard_views.xml',
+
+        # ============================== MENU =================================
+
     ],
+    'qweb': ['static/src/xml/*.xml'],
     'installable': True,
     'auto_install': False,
-    'application': True,
+    'application': False,
 }
+
