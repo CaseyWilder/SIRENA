1ccbc49521c5ff50efcf8c8abf1ecad032f2741e
Thang Nguyen <thang.nguyen@novobi.com>
2021-08-05 16:06:54 -0600

    [SRN-94] FedEx Ground for all return labels

--- novobi_addons/sirena_fedex/models/stock_picking.py
+++ novobi_addons/sirena_fedex/models/stock_picking.py
@@ -19,10 +19,12 @@ class StockPicking(models.Model):
                     delivery_carrier = self.env['delivery.carrier'].search([('id', '=', dc_id)])
                     if delivery_carrier.fedex_service_type == 'GROUND_HOME_DELIVERY':
                         self.delivery_carrier_id = delivery_carrier
+                        self.is_residential_address = True
             else:
                 for dc_id in fedex.delivery_carrier_ids.ids:
                     delivery_carrier = self.env['delivery.carrier'].search([('id', '=', dc_id)])
                     if delivery_carrier.fedex_service_type == 'FEDEX_GROUND':
                         self.delivery_carrier_id = delivery_carrier
+                        self.is_residential_address = False
 
             return result
