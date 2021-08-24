9758d687257ac3224dcc80a2b0a123bbce1eef3f
Tien Nguyen <tien.nguyen@novobi.com>
2021-08-24 09:29:01 +0700

    [SRN-107] Hide products list in Paradise (#34)
    * [SRN-107] Hide products list in Paradise
    
    * [SRN-107] Hide products list in Paradise

--- novobi_addons/sirena_product/models/__init__.py
+++ novobi_addons/sirena_product/models/__init__.py
@@ -1,3 +1,4 @@
+from . import res_company
 from . import product_template
 from . import product_packaging
 from . import stock_picking
--- novobi_addons/sirena_product/models/product_template.py
+++ novobi_addons/sirena_product/models/product_template.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.osv import expression
 
 
 class ProductTemplate(models.Model):
@@ -11,6 +12,18 @@ class ProductTemplate(models.Model):
     part_code = fields.Char(string="Part Code")
 
     packaging_id = fields.Many2one('product.packaging', string='Custom Package')
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        """
+        Hide shared product list, e.g if current company is Paradise (hide_shared_products=True)
+        """
+        hide_companies = self.env['res.company'].sudo().search([('hide_shared_products', '=', True)])
+
+        if not (set(self.env.companies.ids) - set(hide_companies.ids)):
+            args = [('id', '=', -1)]
+
+        return super()._search(args, offset, limit, order, count, access_rights_uid)
 
 
 class ProductProduct(models.Model):
