3670688df4190c97aa0e05a91edd5aedd656bc87
sang-tran-thanh-novobi <sang.tran@novobi.com>
2021-08-30 11:03:43 +0700

    [SRN-119] add tax configuratoin for canada + tax creating adjustment

--- novobi_addons/account_tax_python_ept_customized/models/account_tax.py
+++ novobi_addons/account_tax_python_ept_customized/models/account_tax.py
@@ -9,13 +9,13 @@ class AccountTax(models.Model):
         """
         Override: Add company when creating Amazon Tax, price_include = False
         """
-        company_id = self.env.ref('base.main_company').id,
+        company_id = self.env.ref('sirena_base.sirena_inc_ottawa').id,
         amz_tax_id = self.search(
             [('company_id', '=', company_id),
              ('python_compute', '=', 'result = (price_unit * quantity * line_tax_amount_percent) / 100')])
         if not amz_tax_id:
             tax_vals = {
-                'name': 'Amazon Tax',
+                'name': 'Canada Amazon Tax',
                 'amount_type': 'code',
                 'type_tax_use': 'sale',
                 'amount': 0.00,
