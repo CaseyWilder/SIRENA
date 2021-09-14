0f4f4470111d85585f6d1e9c57eb2327d246d395
Tien Nguyen <tien.nguyen@novobi.com>
2021-09-14 18:49:08 +0700

    [SRN-143] User can create check payments for commission report

--- novobi_addons/sirena_commission_report/__manifest__.py
+++ novobi_addons/sirena_commission_report/__manifest__.py
@@ -10,19 +10,25 @@
     'author': 'Novobi LLC',
     "license": "OPL-1",
     'website': 'https://www.novobi.com',
-    'depends': ['sale', 'amazon_ept'],
+    'depends': ['sale', 'amazon_ept', 'account_check_printing'],
     'data': [
         # ============================== DATA =================================
 
         # ============================== SECURITY =============================
         'security/ir.model.access.csv',
+
         # ============================== VIEWS ================================
+        'views/account_payment_views.xml',
+        'views/sale_order_line_views.xml',
         'views/commission_list_views.xml',
         'views/commission_report_views.xml',
+        'views/res_config_settings_views.xml',
+
         # ============================== REPORT ===============================
 
         # ============================== WIZARD ===============================
         'wizard/generate_commission_report_wizard_views.xml',
+
         # ============================== MENU =================================
         'views/menuitem.xml'
     ],
