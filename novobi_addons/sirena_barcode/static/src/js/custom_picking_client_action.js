0411f7e14628bd8a94a3e13b05c7ec62910ad0a6
Thang Nguyen <thang.nguyen@novobi.com>
2021-09-30 02:11:54 -0600

    [REVERT] Reverted SRN-156

--- novobi_addons/sirena_barcode/static/src/js/custom_picking_client_action.js
+++ novobi_addons/sirena_barcode/static/src/js/custom_picking_client_action.js
@@ -274,6 +274,9 @@ odoo.define('sirena_barcode.custom_picking_client_action', function (require) {
                     return Promise.reject();
                 }
                 def = Promise.resolve({lot_name: barcode, product: product});
+            } else if (! this.currentState.use_create_lots &&
+                    this.currentState.use_existing_lots) {
+                def = searchRead(barcode);
             } else {
                 def = searchRead(barcode).then(function (res) {
                     return Promise.resolve(res);
