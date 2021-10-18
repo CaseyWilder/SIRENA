69c8b6ba527f1942c29ba958f00432663b6a027f
thangnguyen-novobi <84591142+thangnguyen-novobi@users.noreply.github.com>
2021-10-18 13:00:37 -0600

    [SRN-146] Updated implementation for UPS Capital Insurance (#93)
    * [SRN-146] Implemented UPS Capital Insurance integration
    
    * [SRN-146] Renamed Shipping Account's Insurance section
    
    * [SRN-146] Updated implementation for UPS Capital Insurance

--- novobi_addons/sirena_capital_insurance/models/stock_picking.py
+++ novobi_addons/sirena_capital_insurance/models/stock_picking.py
@@ -91,31 +91,23 @@ class StockPicking(models.Model):
                                                     client_secret=self.shipping_account_id.capital_client_secret or '',
                                                     partner_id=self.shipping_account_id.capital_partner_id or '')
 
-        if self.is_create_label and self.fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND']:
-            if not self.capital_insurance_quote_id:
-                log_message = (_("Shipping Option 1<br/>Failed to confirm UPS Capital Insurance: Missing Insurance Quote ID!"))
+        if self.is_create_label and self.fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND'] and self.capital_insurance_quote_id:
+            insurance_result = insurance_request.confirm_insurance_quote(self.capital_insurance_quote_id, self.carrier_tracking_ref if not self.is_mul_packages else self.picking_package_ids[0].carrier_tracking_ref)
+            if insurance_result['result'] == 'ERROR':
+                log_message = (_("Shipping Option 1<br/>Failed to confirm UPS Capital Insurance: %s") % (insurance_result['error_message']))
                 self.message_post(body=log_message)
-            else:
-                insurance_result = insurance_request.confirm_insurance_quote(self.capital_insurance_quote_id, self.carrier_tracking_ref if not self.is_mul_packages else self.picking_package_ids[0].carrier_tracking_ref)
-                if insurance_result['result'] == 'ERROR':
-                    log_message = (_("Shipping Option 1<br/>Failed to confirm UPS Capital Insurance: %s") % (insurance_result['error_message']))
-                    self.message_post(body=log_message)
-                elif insurance_result['result'] == 'SUCCESS':
-                    log_message = (_("UPS Capital Insurance is confirmed for shipping option 1"))
-                    self.message_post(body=log_message)
+            elif insurance_result['result'] == 'SUCCESS':
+                log_message = (_("UPS Capital Insurance is confirmed for shipping option 1"))
+                self.message_post(body=log_message)
 
-        if self.second_is_create_label and self.second_fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND']:
-            if not self.second_capital_insurance_quote_id:
-                log_message = (_("Shipping Option 2<br/>Failed to confirm UPS Capital Insurance: Missing Insurance Quote ID!"))
+        if self.second_is_create_label and self.second_fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND'] and self.second_capital_insurance_quote_id:
+            second_insurance_result = insurance_request.confirm_insurance_quote(self.second_capital_insurance_quote_id, self.second_carrier_tracking_ref)
+            if second_insurance_result['result'] == 'ERROR':
+                log_message = (_("Shipping Option 2<br/>Failed to confirm UPS Capital Insurance: %s") % (second_insurance_result['error_message']))
                 self.message_post(body=log_message)
-            else:
-                second_insurance_result = insurance_request.confirm_insurance_quote(self.second_capital_insurance_quote_id, self.second_carrier_tracking_ref)
-                if second_insurance_result['result'] == 'ERROR':
-                    log_message = (_("Shipping Option 2<br/>Failed to confirm UPS Capital Insurance: %s") % (second_insurance_result['error_message']))
-                    self.message_post(body=log_message)
-                elif second_insurance_result['result'] == 'SUCCESS':
-                    log_message = (_("UPS Capital Insurance is confirmed for shipping option 2"))
-                    self.message_post(body=log_message)
+            elif second_insurance_result['result'] == 'SUCCESS':
+                log_message = (_("UPS Capital Insurance is confirmed for shipping option 2"))
+                self.message_post(body=log_message)
 
         return result
 
@@ -138,38 +130,30 @@ class StockPicking(models.Model):
                                                     client_secret=self.shipping_account_id.capital_client_secret or '',
                                                     partner_id=self.shipping_account_id.capital_partner_id or '')
 
-        if self.is_void_first_label and fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND']:
-            if not self.capital_insurance_quote_id:
-                log_message = (_("Shipping Option 1<br/>Failed to void UPS Capital Insurance: Missing Insurance Quote ID!"))
+        if self.is_void_first_label and fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND'] and self.capital_insurance_quote_id:
+            insurance_result = insurance_request.void_insurance_quote(self.capital_insurance_quote_id, carrier_tracking_ref)
+            if insurance_result['result'] == 'ERROR' and 'Void is not allowed on unconfirmed quote' not in insurance_result['error_message']:
+                log_message = (_("Shipping Option 1<br/>Failed to void UPS Capital Insurance: %s") % (insurance_result['error_message']))
                 self.message_post(body=log_message)
-            else:
-                insurance_result = insurance_request.void_insurance_quote(self.capital_insurance_quote_id, carrier_tracking_ref)
-                if insurance_result['result'] == 'ERROR':
-                    log_message = (_("Shipping Option 1<br/>Failed to void UPS Capital Insurance: %s") % (insurance_result['error_message']))
-                    self.message_post(body=log_message)
-                elif insurance_result['result'] == 'SUCCESS':
-                    self.update({
-                        'capital_insurance_quote_id': False,
-                        'capital_insurance_premium_amount': False
-                    })
-                    log_message = (_("UPS Capital Insurance for shipping option 1 has been voided."))
-                    self.message_post(body=log_message)
+            elif insurance_result['result'] == 'SUCCESS' or 'Void is not allowed on unconfirmed quote' in insurance_result.get('error_message', ''):
+                log_message = (_("UPS Capital Insurance for shipping option 1 has been voided."))
+                self.message_post(body=log_message)
+            self.update({
+                'capital_insurance_quote_id': False,
+                'capital_insurance_premium_amount': False
+            })
 
-        if self.is_void_second_label and second_fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND']:
-            if not self.second_capital_insurance_quote_id:
-                log_message = (_("Shipping Option 2<br/>Failed to void UPS Capital Insurance: Missing Insurance Quote ID!"))
+        if self.is_void_second_label and second_fedex_service_type in ['GROUND_HOME_DELIVERY', 'FEDEX_GROUND'] and self.second_capital_insurance_quote_id:
+            second_insurance_result = insurance_request.void_insurance_quote(self.second_capital_insurance_quote_id, second_carrier_tracking_ref)
+            if second_insurance_result['result'] == 'ERROR' and 'Void is not allowed on unconfirmed quote' not in second_insurance_result['error_message']:
+                log_message = (_("Shipping Option 2<br/>Failed to void UPS Capital Insurance: %s") % (second_insurance_result['error_message']))
                 self.message_post(body=log_message)
-            else:
-                second_insurance_result = insurance_request.void_insurance_quote(self.second_capital_insurance_quote_id, second_carrier_tracking_ref)
-                if second_insurance_result['result'] == 'ERROR':
-                    log_message = (_("Shipping Option 2<br/>Failed to void UPS Capital Insurance: %s") % (second_insurance_result['error_message']))
-                    self.message_post(body=log_message)
-                elif second_insurance_result['result'] == 'SUCCESS':
-                    self.update({
-                        'second_capital_insurance_quote_id': False,
-                        'second_capital_insurance_premium_amount': False
-                    })
-                    log_message = (_("UPS Capital Insurance for shipping option 2 has been voided."))
-                    self.message_post(body=log_message)
+            elif second_insurance_result['result'] == 'SUCCESS' or 'Void is not allowed on unconfirmed quote' in second_insurance_result.get('error_message', ''):
+                log_message = (_("UPS Capital Insurance for shipping option 2 has been voided."))
+                self.message_post(body=log_message)
+            self.update({
+                'second_capital_insurance_quote_id': False,
+                'second_capital_insurance_premium_amount': False
+            })
 
         return result
